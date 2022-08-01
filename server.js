0b40115c9e63bd5a1c54272fa73edfeac8a3416e
kylieogg <kylieogg1@gmail.com>
2022-08-01 10:32:04 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
-
 const app =express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+mongoose.set('debug', true);
 
 app.use(require('./routes/api.js'));
 
